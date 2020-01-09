54bfd84e92e8a88884afd5a435c78ce78e836e36
migCruQue <miguel.cruz.quesada@gmail.com>
2020-01-09 17:57:12 +0000

    2020/footer removednav changes ....

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+// let nav = document.getElementById('nav');
+//
+// width: 50%;
+//
+//
+// margin: 10px;
+// text-align: center;
+// display: flex;
+// flex-direction: column;
+//
+// to select the button, nav and container using Jquery and to change the CSS using jquery css method.
