8f97f14746f24f68129785847c2fe6fd96df9445
migCruQue <miguel.cruz.quesada@gmail.com>
2020-01-15 16:14:43 +0000

    a few changes in the main nav

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@
 // flex-direction: column;
 //
 // to select the button, nav and container using Jquery and to change the CSS using jquery css method.
+
+//
+$('button.navbar-toggler').on('click', function(){
+  if ($('button.navbar-toggler').attr('aria-expanded') == 'false') {
+      $('nav').addClass('narrow');
+      $('#nav-container').addClass('narrow');
+      $('a').addClass('mx-auto');
+      }
+
+  else {
+
+    $('nav').removeClass('narrow');
+    $('#nav-container').removeClass('narrow');
+    $('a').removeClass('mx-auto');
+  }
+
+});
