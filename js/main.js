1b191a7c10c3e2bd5102d559dbad1b29b1ef40ec
migCruQue <miguel.cruz.quesada@gmail.com>
2020-01-22 16:28:15 +0000

    contact me tab updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 
 // CUSTOMIZE THE MENU WHEN IT IS EXPANDED THROUGH THE TOOGLER BUTTON.
+
 $('button.navbar-toggler').on('click', function(){
   if ($('button.navbar-toggler').attr('aria-expanded') == 'false') {
       $('nav').addClass('narrow');
@@ -17,9 +18,25 @@ $('button.navbar-toggler').on('click', function(){
 
 });
 
+// CUSTOMIZE THE MENU WHEN IT IS EXPANDED THROUGH THE TOOGLER BUTTON.
+
 
 // TRYING TO FIX THE SIZE OF THE MAIN DIV
+
 // set the main div height to the window inner height.
 // $('main').height(Math.floor(window.innerHeight * 0.87));
 
+// set the main div height to the window inner height in the contact me tab.
+// $('div#contact-me').height(Math.floor($(window).height() * 0.95));
+
+// TRYING TO FIX THE SIZE OF THE MAIN DIV
+
+
+
+// POSITIONING THE BOX(WHERE I DISPLAY SOME MESSAGES) IN THE MAIN INDEX.
+
 $('#box').offset({top: Math.floor($(window).height() * 0.7)});
+
+// POSITIONING THE BOX(WHERE I DISPLAY SOME MESSAGES) IN THE MAIN INDEX.
+
+$('#contact-me-box').offset({top: Math.floor($(window).height() * 0.25)});
