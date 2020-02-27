e8d6a2302c8d4cb9cafcd9e26b5b8d06a4843525
migCruQue <miguel.cruz.quesada@gmail.com>
2020-02-27 17:37:34 +0000

    css and img folders arranged plus projects layout updated

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// POSITIONING THE BOX(WHERE I DISPLAY SOME MESSAGES) IN THE MAIN INDEX.
+
+$('#box').offset({top: Math.floor($(window).height() * 0.7)});
+
+// POSITIONING THE BOX(WHERE I DISPLAY SOME MESSAGES) IN THE MAIN INDEX.
+
+$('#contact-me-box').offset({top: Math.floor($(window).height() * 0.25)});
+
+
+// EVENT HANDLER TRIGGERS EVERY TIME THE WINDOWS IS RESIZE.
+$( window ).resize(function() {
+
+    if($("a[href='index.html']").hasClass('currentTab')){
+      $('#box').offset({top: Math.floor($(window).height() * 0.7)});
+
+    }
+
+    if($("a[href='contactMe.html']").hasClass('currentTab')){
+      $('#contact-me-box').offset({top: Math.floor($(window).height() * 0.25)});
+
+    }
+
+});
