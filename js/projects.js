972ce1eab69693df5f3e7cac21d2055dbbacefaa
migCruQue <miguel.cruz.quesada@gmail.com>
2020-06-19 16:49:31 +0100

    first changes in project branch/tab

--- js/projects.js
+++ js/projects.js
@@ -4,16 +4,16 @@
 
 
 // CHANGE THE PROJECT-CARD APPEARENCE WHEN MOUSE OVER SHOWING A DESCRIPTION OF THE PROJECT.
-$('.project-card').mouseenter(function(){
-  $(this).attr("style","background-image: none");
-  $(this).addClass('description');}
+// $('.project-card').mouseenter(function(){
+//   $(this).attr("style","background-image: none");
+//   $(this).addClass('description');}
 
-);
+// );
 
 
 // GOES BACK TO THE DEFAULT APPEARENCE WITH THE PIC
-$('.project-card').mouseleave(function(){
-  $(this).removeAttr("style");
-  $(this).removeClass('description');}
+// $('.project-card').mouseleave(function(){
+//   $(this).removeAttr("style");
+//   $(this).removeClass('description');}
 
-);
+// );
