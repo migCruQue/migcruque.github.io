fc4c38220e38f0cabd81032444acb899e80fe991
Miguel Cruz Quesada <miguel.cruz.quesada@gmail.com>
2021-10-14 17:41:49 +0100

    refactoring nav functionality

--- src/modules/nav.js
+++ src/modules/nav.js
@@ -10,18 +10,17 @@ let WidthGreaterThan1000 = $(window).width() > 1000;
 
 function setSuitableNav() {          //on loading set the nav with the class hidden or grid depending on the window viewport width
     if ($(window).width() >= 1000) {
+        setTabFitting()
         $nav.removeClass('hidden').addClass('nav_landscape');
         $hamburger.addClass('hidden');
     } 
 }
 
-
-
-
 function resizeNav(){
         if ( !WidthGreaterThan1000 && $(window).width() > 1000){
-            $nav.addClass('nav_landscape').removeClass('hidden');
+            $nav.addClass('nav_landscape').removeClass('hidden nav_portrait');
             $hamburger.addClass('hidden');
+            $exit_menu.addClass('hidden');
         } 
         if (WidthGreaterThan1000 && $(window).width() < 1000){
             $nav.addClass('hidden').removeClass('nav_landscape');
@@ -43,16 +42,25 @@ function exit_menuFunctionality(){
     $('.hamburger').removeClass('hidden');
 }
 
+
+function setTabFitting(){
+    let currentTab = $div_tab.not('.hidden');
+    if(($(window).height() * 1.2) < $(document).height()) {               // I multiply the window height by 1.2 as I still want to set
+        currentTab.addClass('alignself');                             // the tab fitting in the screen to the following
+    } else {currentTab.addClass('height100');}
+}
+
 function nav_buttonsFunctionality(e){
     if($nav.hasClass('nav_portrait')){
         $nav.addClass('hidden').removeClass('nav_portrait');// * Check if nav has class nav_portrait and it hides it.
     }  
-    $div_tab.not('.hidden').addClass('hidden'); // * hide the previous tab
-    $nav_button.filter('.hidden').removeClass('hidden'); // * shows the previous button
+    $div_tab.not('.hidden').addClass('hidden').removeClass('height100 alignself'); // * hide the previous tab
+    $nav_button.filter('.hidden').not('#exit_menu').removeClass('hidden'); // * shows the previous button
     let buttonID = e.target.id;
     $(`#${buttonID}`).addClass('hidden'); // * Add class "hidden" to the currently selected button  
     let tabID = buttonID.replace('_button', '');
     $(`#${tabID}`).removeClass('hidden'); // * Remove class "hidden" to the currently selected tab 
+    setTabFitting();
 }
 
 
