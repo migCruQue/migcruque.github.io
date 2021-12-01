4d6a9c6bb534b873f6fcd25c948bff22f09e6e37
Miguel Cruz Quesada <miguel.cruz.quesada@gmail.com>
2021-12-01 16:18:18 +0000

    jquery removed

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import * as bootstrap from 'bootstrap';
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> NAV FUNCTIONALITY >>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 /* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 
-const $nav = $('nav');
+const nav = document.querySelector('nav');
 
 let tabReference = document.querySelector('.tab-reference');
 let tabPaneActive = document.querySelector('.tab-pane.active');
 let tabPaneID = tabPaneActive.getAttribute("id");
-
-console.log(tabPaneID);
 
 const imageTabData = {
     "home": {
@@ -40,22 +38,26 @@ const imageTabData = {
 };
 
 // when hamburger toggles add or remove .overlapping (adding background-color to nav, ovelapping the body) 
-$(".navbar-toggler").on("click", function() {
-   $nav.hasClass("overlapping") ? $nav.removeClass("overlapping") : $nav.addClass("overlapping");
-  });
+const navbarToggler = document.querySelector('.navbar-toggler');
 
-
-let isCurrentWidthSmall = ($(window).width()) <= 992 ;  // check the window screen size and assing True or False whether the screen size is smaller than 992px
-
-$(window).on('resize', () => {
-    if( $(window).width() > 992  && $nav.hasClass("ovelapping")){  // Remove .overlapping from the nav when resize to a big screen.
-        $nav.removeClass("ovelapping")}
-
-    isCurrentWidthSmall = ($(window).width()) <= 992; 
+navbarToggler.addEventListener("click", function() {
+    console.log("navbarToggler clicked");
+    nav.classList.contains("overlapping") ? nav.classList.remove("overlapping") : nav.classList.add("overlapping");
 });
 
 
-const $navLinks = $('.nav-item');
+let isCurrentWidthSmall = window.innerWidth <= 992 ;  // check the window screen size and assing True or False whether the screen size is smaller than 992px
+
+window.addEventListener('resize', () => {
+    if( window.innerWidth > 992  && nav.classList.contains("overlapping")){  // Remove .overlapping from the nav when resize to a big screen.
+        nav.classList.remove("overlapping")}
+        console.log('resize done');
+
+    isCurrentWidthSmall = window.innerWidth <= 992; 
+});
+
+
+const navLinks = document.querySelectorAll('.nav-item');
 const navbarCollapse  = document.getElementById('navbarSupportedContent');
 
 function menuToggle() {
@@ -63,18 +65,18 @@ function menuToggle() {
     bsCollapse.toggle();
 }
 
-$navLinks.on("click", (e) => {    // Use the toggle method in a new boostrap.Collapse(menuToggle)  
-    let buttonACAttr = e.target.getAttribute("aria-controls");
+navLinks.forEach(item => item.addEventListener("click", function(e) {    
+    let buttonACAttr = e.target.getAttribute("aria-controls"); // set the src and attr attributes tab reference image when a nav link is click
     if(buttonACAttr !== tabPaneID){
         tabReference.setAttribute("src", imageTabData[buttonACAttr].src);
         tabReference.setAttribute("alt", imageTabData[buttonACAttr].alt);
         tabPaneID = buttonACAttr;
-        console.log(tabPaneID, buttonACAttr);
     }
-    $nav.hasClass("ovelapping") ? $nav.removeClass("ovelapping") : $nav.addClass("ovelapping");
-    menuToggle();//to collapse the navbar-collapse when any button from the menu is click
+    nav.classList.contains("overlapping") ? nav.classList.remove("overlapping") : nav.classList.add("overlapping"); // remove or add the overlapping class to the menu in small screens.
+    menuToggle();//to collapse the navbar-collapse when any button from the menu is click);a
     
-});
+    })
+);
 
 
 
@@ -85,56 +87,6 @@ $navLinks.on("click", (e) => {    // Use the toggle method in a new boostrap.Col
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
 /* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> NAV FUNCTIONALITY >>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 /* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
-
-
-// * CONTACT_ME.JS
-
-
-//Movement Animation to happen
-const cards = document.querySelectorAll('.contact_card');
-const circle_backgroun = document.querySelector('.circle_background');
-const title = document.querySelector('.title');
-const contact_logo = document.querySelector('.contact_logo img');
-
-// Moving Animation Event
-// card.addEventListener('mouseover', (e) => {
-//   let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-//   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-//   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-// });
-
-
-// Animate In
-cards.forEach(card => {
-    card.addEventListener("mouseenter", (e) => {
-        // card.style.transform = `rotateY(25deg) rotateX(25deg)`;
-        // //Pop out
-        // title.style.transform = "translateZ(150px)";
-        contact_logo.style.transform = "translateZ(200px) rotateZ( -45deg)";
-    });
-});
-
-
-cards.forEach(card => {
-    card.addEventListener("mouseleave", (e) => {
-        // card.style.transition = "all 0.5a ease";
-        // card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-        // //Pop back
-        // title.style.transform = "translateZ(0px)";
-        contact_logo.style.transform = "translateZ(0px) rotateZ( 0deg)";
-    });
-});
-
-
-//Animate Out
-// card.addEventListener('mouseleave', (e) => {
-//   card.style.transition = "all 0.5a ease";
-//   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-//   //Pop back
-//   title.style.transform = "translateZ(0px)";
-//   contact_logo.style.transform = "translateZ(0px) rotateZ( 0deg)";
-// });
-
 
 
 // * TYPEWRITER.JS 
